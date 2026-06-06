e99b78906d4e505ce230d16ff37db9b60e4abeaa
Saikat Ghosh <saikat.blast20@gmail.com>
2026-06-07 02:19:48 +0600

    link-added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+ origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : [],
   credentials: true
 }));
 app.use(express.json());
